e35d9aa316bb6b4dbf7863ea361e4ba39b8f9ad1
Paweł Łysik <pawel.lysik@onet.pl>
2020-08-03 09:17:43 +0200

    2 changes 1. logarithmic amplitude on spectrogram 2. difference diagram (amplitude and frequency over time)

--- main.py
+++ main.py
@@ -28,16 +28,30 @@ for i in range(fouriers):
 	dft = np.fft.rfft(data)
 	for j in range(len(data)//2):
 		#print(freq[j], ":", cmath.polar(dft[j])[0])
-		freqRow.append(cmath.polar(dft[j])[0])
+		freqRow.append(np.log2(cmath.polar(dft[j])[0]+1))
 	#print("/////////////////////////////")
 	freqTab.append(freqRow)
-
+	
+#spectrogram
 amp = np.array(freqTab).transpose()
+plt.subplot(2, 1, 1)
 plt.pcolormesh(np.arange(0, fouriers/20, 1/20), np.arange(0,22040,20), amp)
 plt.ylabel('frequency [Hz]')
 plt.xlabel('time [s]')
-plt.show()
 
+
+#difference diagram
+diffTable = []
+for i in range(fouriers-1):
+	sum = 0
+	for j in range(len(data)//2):
+		sum += abs(freqTab[i+1][j]-freqTab[i][j])/(freqTab[i+1][j]+freqTab[i][j]+1)
+	diffTable.append(sum)
+plt.subplot(2, 1, 2)
+plt.plot(np.arange(0, (fouriers-1)/20, 1/20), diffTable)
+
+plt.show()
+	
 
 
     
