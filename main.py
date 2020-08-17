94597581553fb731fb1fd70f823cf65fed5d72de
Wernychora <pawe.lysik@onet.pl>
2020-08-17 09:27:13 +0200

    FFT on spectogram -fft on spectogram columns (fft for each 50ms column) -second spectrogram (for new fft) -new program syntax (second argument is mode: "spectograms", "diffPlots")

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def read_file(data):
 		probe = struct.unpack("<2h", waveData)
 		data.append(probe[0]) #channel 0, quit channel 1
 
-def fft(freqTab, toneTab):
+def fft(freqTab, dft2Tab, toneTab):
 	fourierStep = int(frameRate*fourierSection/1000)
 	for key, val in contraOctave.items():
 		fourierDuration = 1/val
@@ -38,7 +38,13 @@ def fft(freqTab, toneTab):
 		for j in range(minimalToneFreqRowSize):
 			for tone, toneFreqRowArray in toneTab.items():
 				freqRow.append(toneFreqRowArray[i][j])
-		freqTab.append(freqRow)	
+		freqTab.append(freqRow)
+	
+	#fft2
+	for i in range(fouriers):
+		fftCol = []
+		dft2 = np.fft.rfft(freqTab[i])
+		dft2Tab.append(dft2)
 	
 	return minimalToneFreqRowSize
 
@@ -64,7 +70,7 @@ def diffPlot(array, flagArray, title, nr, bottomMargin, topMargin, differentiato
 	plt.ylim(bottomVal,topVal)
 	plt.xlabel("time [s]")
 
-def display(freqTab, toneTab, toneFourierSamples):
+def display(freqTab, dft2Tab, toneTab, toneFourierSamples, mode):
 	#spectrogram data
 	specAxisY = []
 	for i in range(toneFourierSamples):
@@ -72,45 +78,62 @@ def display(freqTab, toneTab, toneFourierSamples):
 			specAxisY.append(freq*i)		
 	amp = np.array(freqTab).transpose()
 
-	#difference diagram
+	#diffPlots
 	diffArray = []
 	diffFlags = []
-	for i in range(fouriers-1):
-		sum = 0
-		for j in range(int(len(specAxisY)/10), len(specAxisY)):
-			sum += abs(freqTab[i+1][j]-freqTab[i][j])/(freqTab[i+1][j]+freqTab[i][j]+1)
-		diffArray.append(sum)	
-	diffPlot(diffArray, diffFlags, "Difference diagram", 2, 0.01, 0.99)
-	
-	#diff derivative diagram
-	diffPrim = np.gradient(diffArray, edge_order = 2)
-	diffPrimFlags = [False] * len(diffArray)
-	diffPlot(diffPrim, diffPrimFlags, "Derivative diagram", 3, 0.01, 0.99, "zeroPoint")
-	
-	#diff second derivative diagram
-	diffBis = np.gradient(diffPrim, edge_order = 2)
-	diffBisFlags = [False] * len(diffArray)
-	diffPlot(diffBis, diffBisFlags, "Second derivative diagram", 4, 0.02, 0.98, "negativePoint")
-	
+	if mode == "diffPlots":
+		#difference plot
+		for i in range(fouriers-1):
+			sum = 0
+			for j in range(int(len(specAxisY)/10), len(specAxisY)):
+				sum += abs(freqTab[i+1][j]-freqTab[i][j])/(freqTab[i+1][j]+freqTab[i][j]+1)
+			diffArray.append(sum)	
+		diffPlot(diffArray, diffFlags, "Difference diagram", 2, 0.01, 0.99)
+		
+		#diff derivative plot
+		diffPrim = np.gradient(diffArray, edge_order = 2)
+		diffPrimFlags = [False] * len(diffArray)
+		diffPlot(diffPrim, diffPrimFlags, "Derivative diagram", 3, 0.01, 0.99, "zeroPoint")
+		
+		#diff second derivative plot
+		diffBis = np.gradient(diffPrim, edge_order = 2)
+		diffBisFlags = [False] * len(diffArray)
+		diffPlot(diffBis, diffBisFlags, "Second derivative diagram", 4, 0.02, 0.98, "negativePoint")
+		
 	#spectrogram display
-	plt.subplot(2, 2, 1)
+	if mode == "diffPlots": plt.subplot(2, 2, 1)
+	if mode == "spectograms": plt.subplot(2, 1, 1)
 	plt.pcolormesh(np.arange(0, fouriers/20, 1/20), specAxisY, amp, shading="nearest")
-	for x in range(2, len(diffArray)-2):
-		if diffPrimFlags[x] and (diffBisFlags[x-2] or diffBisFlags[x-1] or diffBisFlags[x] or diffBisFlags[x+1]):
-			plt.axvline(x/20, 0, 10000, label='pyplot vertical line', color="red")
+	if mode == "diffPlots":
+		for x in range(2, len(diffArray)-2):
+			if diffPrimFlags[x] and (diffBisFlags[x-2] or diffBisFlags[x-1] or diffBisFlags[x] or diffBisFlags[x+1]):
+				plt.axvline(x/20, 0, 10000, label='pyplot vertical line', color="red")
 	plt.title("Spectrogram")
 	plt.xlim(0,15)
 	plt.ylim(0,11000)
 	plt.ylabel("frequency [Hz]")
 	plt.xlabel("time [s]")
 	
+	#fft2 spectrogram
+	if mode == "spectograms":
+		dft2Data = np.array(dft2Tab).transpose()
+		plt.subplot(2, 1, 2)
+		plt.pcolormesh(np.arange(0, fouriers/20, 1/20), np.arange(0, len(specAxisY)), amp, shading="nearest")
+		plt.title("DFT2 spectrogram")
+		plt.xlim(0,15)
+		plt.ylim(0,len(specAxisY))
+		#plt.ylabel("frequency [Hz]")
+		plt.xlabel("time [s]")
+		
 	plt.show()
 	
 #####
 	
-if len(sys.argv) != 2 :
-	print ("Program syntax: main.py [name of *.wav file]")
+if len(sys.argv) != 3:
+	print ("Program syntax: main.py [name of *.wav file] [spectograms | diffPlots]")
 	exit()
+	
+mode = sys.argv[2]
 
 contraOctave = {"C" : 32.70, "Cis" : 34.65, "D" : 36.71, "Dis" : 38.89,
 "E" : 41.20, "F" : 43.65, "Fis" : 46.25, "G" : 49.00, "Gis" : 51.91,
@@ -129,8 +152,9 @@ read_file(data)
 
 #fft
 freqTab = []
+dft2Tab = []
 toneTab = { i : [] for i in contraOctave.keys() }
-toneFourierSamples = fft(freqTab, toneTab)
+toneFourierSamples = fft(freqTab, dft2Tab, toneTab)
 
-#spectrogram + difference plot
-display(freqTab, toneTab, toneFourierSamples)
+#spectrogram + difference plot (modes: "spectograms", "diffPlots")
+display(freqTab, dft2Tab, toneTab, toneFourierSamples, mode)
